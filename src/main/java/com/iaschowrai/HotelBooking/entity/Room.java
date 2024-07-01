f829312a9cdcc6e28f4f2d9138aeaa6591a8ea97
Irshad Ahmed <iaschowrai@gmail.com>
2024-07-01 19:04:07 -0400

    Room pojo is added

--- src/main/java/com/iaschowrai/HotelBooking/entity/Room.java
+++ src/main/java/com/iaschowrai/HotelBooking/entity/Room.java
@@ -1,2 +1,41 @@
-package com.iaschowrai.HotelBooking.entity;public class Room {
+package com.iaschowrai.HotelBooking.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String bookingConfirmation;
+    private String roomType;
+    private String roomNumber;
+    private BigDecimal roomPrice;
+    private String roomPhotoURL;
+    private String roomDescription;
+    private List<Booking> bookings = new ArrayList<>();
+
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", bookingConfirmation='" + bookingConfirmation + '\'' +
+                ", roomType='" + roomType + '\'' +
+                ", roomNumber='" + roomNumber + '\'' +
+                ", roomPrice=" + roomPrice +
+                ", roomPhotoURL='" + roomPhotoURL + '\'' +
+                ", roomDescription='" + roomDescription + '\'' +
+                ", bookings=" + bookings +
+                '}';
+    }
 }
