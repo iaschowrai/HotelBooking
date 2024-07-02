79c1db3543d8960f53b0baa5de7e0fd9abb3031f
Irshad Ahmed <iaschowrai@gmail.com>
2024-07-01 22:35:48 -0400

    Cors configuration bean is created for MVC mapping

--- src/main/java/com/iaschowrai/HotelBooking/security/CorsConfig.java
+++ src/main/java/com/iaschowrai/HotelBooking/security/CorsConfig.java
@@ -1,2 +1,23 @@
-package com.iaschowrai.HotelBooking.security;public class CorsConfig {
+package com.iaschowrai.HotelBooking.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*");
+            }
+
+        };
+    }
 }
