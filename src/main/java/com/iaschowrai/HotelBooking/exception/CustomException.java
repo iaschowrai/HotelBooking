60685d574b3ce93e33ddaf99ef4591d03f1d98e3
Irshad Ahmed <iaschowrai@gmail.com>
2024-07-01 20:25:47 -0400

    Basic Custom exception are added

---
+++ src/main/java/com/iaschowrai/HotelBooking/exception/CustomException.java
@@ -0,0 +1,9 @@
+package com.iaschowrai.HotelBooking.exception;
+
+public class CustomException extends RuntimeException {
+
+    public CustomException(String message){
+        super(message);
+    }
+
+}
