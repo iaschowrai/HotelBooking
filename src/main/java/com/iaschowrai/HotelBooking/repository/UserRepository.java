9f97617f9b49efae8a7b4498a391dc67ba50b69a
Irshad Ahmed <iaschowrai@gmail.com>
2024-07-01 20:23:38 -0400

    USER, ROOM and Booking custom queries are added

---
+++ src/main/java/com/iaschowrai/HotelBooking/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.iaschowrai.HotelBooking.repository;
+
+import com.iaschowrai.HotelBooking.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
--- src/main/java/com/iaschowrai/HotelBooking/repository/hotelBookingRepository.java
+++
@@ -1,6 +0,0 @@
-package com.iaschowrai.HotelBooking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface hotelBookingRepository extends JpaRepository<,Long> {
-}
